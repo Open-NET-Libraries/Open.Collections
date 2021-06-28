49e7fe5289eba8e5663f232515b6a1ee04119650
electricessence <5899455+electricessence@users.noreply.github.com>
2021-06-27 19:50:57 -0700

    Better use of arraypool in stream copy.

--- source/Extensions.Stream.cs
+++ source/Extensions.Stream.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
 
 namespace Open.Collections
@@ -8,18 +9,26 @@ namespace Open.Collections
 		/// <summary>
 		/// Copies the source stream to the target.
 		/// </summary>
-		public static void CopyTo(this Stream source, Stream target)
+		public static void CopyTo(this Stream source, Stream target, int bufferSize = 4096, bool clearBufferAfter = false)
 		{
 			if (source is null)
 				throw new NullReferenceException();
 			if (target is null)
 				throw new ArgumentNullException(nameof(target));
 
-			var bytes = System.Buffers.ArrayPool<byte>.Shared.Rent(4096);
-			int cnt;
-			while ((cnt = source.Read(bytes, 0, bytes.Length)) != 0)
-				target.Write(bytes, 0, cnt);
-			System.Buffers.ArrayPool<byte>.Shared.Return(bytes, true);
+			var pool = ArrayPool<byte>.Shared;
+			var bytes = pool.Rent(bufferSize);
+
+			try
+			{
+				int cnt;
+				while ((cnt = source.Read(bytes, 0, bufferSize)) != 0)
+					target.Write(bytes, 0, cnt);
+			}
+			finally
+			{
+				pool.Return(bytes, clearBufferAfter);
+			}
 		}
 	}
 }
