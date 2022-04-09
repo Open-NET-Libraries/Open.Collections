5ceb00b038daae869bdd1ded6d2cfee2530d8152
electricessence <5899455+electricessence@users.noreply.github.com>
2022-04-09 12:02:15 -0700

    Restored 'AddThese' because of potential ambiguity.

--- source/IAddMultiple.cs
+++ source/IAddMultiple.cs
@@ -7,7 +7,7 @@ public interface IAddMultiple<T>
     /// <param name="item1">First item to add.</param>
     /// <param name="item2">Additional item to add.</param>
     /// <param name="items">Extended param items to add.</param>
-    void Add(T item1, T item2, params T[] items);
+    void AddThese(T item1, T item2, params T[] items);
 
     /// <summary>
     /// Adds all the items in <paramref name="items"/> to this collection.
